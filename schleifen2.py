2b1cf7918acb41d652a8cde85f28916360166c1c
vakoo4tw <danielkerger@gmail.com>
2023-12-15 21:48:00 +0100

    schleife 2 added

---
+++ schleifen2.py
@@ -0,0 +1,20 @@
+# Wir legen eine Liste mit drei Objekten an
+früchte = ["Apfel", "Banane", "Orange"]
+
+# Eine Liste kann Eintrag für Eintrag durchlaufen werden, für jeden Eintrag in der Liste
+# wird die Schleife einmal durchlaufen.
+# Die Schleife zerlegt die Liste in seine Einträge und legt den Aktuellen Eintrag in die Variable "frucht"
+# Im ersten Durchlauf der Schleife wird der Text "Apfel" in die Variable Frucht gespeichert und über print(frucht) ausgegeben.
+# Im zweiten Durchlauf wird "Banane" in die Variable Frucht gespeichert und geprintet
+# Im dritten Durchlauf wird "Orange" in die Variable Frucht gespeichert, geprintet und die Schleife wird beendet 
+# weil alle Elemente von früchte durchlaufen wurden
+for frucht in früchte:
+    print(frucht)
+
+
+# Jeder Datentyp der Sequenzierbar ist, kann durchlaufen und in seine Elemente zerlegt werden  
+wort = "wort"
+
+# Hierbei wird jeder Buchstabe von "wort" einzeln durchlaufen und über print(buchstabe) ausgegeben
+for buchstabe in wort:
+    print(buchstabe)
