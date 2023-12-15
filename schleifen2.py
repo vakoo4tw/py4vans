cd01ede9296d794b3b5e60139420e430a7d46c26
vakoo4tw <danielkerger@gmail.com>
2023-12-15 21:50:20 +0100

    fix

--- schleifen2.py
+++ schleifen2.py
@@ -3,7 +3,7 @@ früchte = ["Apfel", "Banane", "Orange"]
 
 # Eine Liste kann Eintrag für Eintrag durchlaufen werden, für jeden Eintrag in der Liste
 # wird die Schleife einmal durchlaufen.
-# Die Schleife zerlegt die Liste in seine Einträge und legt den Aktuellen Eintrag in die Variable "frucht"
+# Die Schleife zerlegt die Liste in seine Einträge und legt den aktuellen Eintrag in die Variable "frucht"
 # Im ersten Durchlauf der Schleife wird der Text "Apfel" in die Variable Frucht gespeichert und über print(frucht) ausgegeben.
 # Im zweiten Durchlauf wird "Banane" in die Variable Frucht gespeichert und geprintet
 # Im dritten Durchlauf wird "Orange" in die Variable Frucht gespeichert, geprintet und die Schleife wird beendet 
